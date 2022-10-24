b9ddcebe16851fc6c61b2dd3a04d9c502a31f858
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-10-24 22:16:56 +0900

    ユーザー管理機能　ビューの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:birth_day,:first_name,:last_name,:read_first,:read_last])
+    end
 end
