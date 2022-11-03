dc48147fbaa237dec5ad974e54217ba8cab668c2
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-03 20:24:35 +0900

    コメント機能実装（途中まで）

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,15 +1,28 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("CommentChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+if(location.pathname.match(/\/items\/\d/)){
+  consumer.subscriptions.create({
+    channel: "CommentChannel",
+    item_id: location.pathname.match(/\d+/)[0]
+  }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+    received(data) {
+      const html = `
+        <div class="comment">
+          <p class="user-info">${data.user.nick_name}： </p>
+          <p>${data.comment.text}</p>
+        </div>`
+      const comments = document.getElementById("comments")
+      comments.insertAdjacentHTML('beforeend', html)
+      const commentForm = document.getElementById("comment-form")
+      commentForm.reset();
+    }
+  })
+}
