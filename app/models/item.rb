2f8c75b078413537d7b12f936beee2d39f987da3
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-06 22:14:45 +0900

    複数枚画像投稿の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
-  validates :product_name, :product_description, :image, presence: true
+  validates :product_name, :product_description, :images,presence: true
 
     validates :price,numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999},
       presence: { messasge: "can't be blank"}
-
+      validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
  
   validates :category_id, :condition_id, :contribution_id, :prefecture_id, :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  has_one_attached :image
+  has_many_attached :images
   belongs_to :user
   has_one :order
   extend ActiveHash::Associations::ActiveRecordExtensions
