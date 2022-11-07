27156e0e53ab73cb2bd433af05d706c648ae9ca6
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-07 16:54:42 +0900

    画像投稿最終調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
 
     validates :price,numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999},
       presence: { messasge: "can't be blank"}
+      validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
 
+    
  
   validates :category_id, :condition_id, :contribution_id, :prefecture_id, :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 
