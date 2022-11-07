6d01ccbe2205dd0405f44ed97726170ab8dfbe6b
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-07 16:41:04 +0900

    差し戻しての修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  validates :product_name, :product_description, :image, presence: true
+  validates :product_name, :product_description, :images, presence: true
 
     validates :price,numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999},
       presence: { messasge: "can't be blank"}
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
  
   validates :category_id, :condition_id, :contribution_id, :prefecture_id, :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  has_one_attached :image
+  has_many_attached :images
   belongs_to :user
   has_one :order
   extend ActiveHash::Associations::ActiveRecordExtensions
